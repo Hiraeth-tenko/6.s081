a154b2788dd03e80dca96027859ed77c67568e67
Hiraeth <18950365798@163.com>
2024-02-14 21:34:09 +0800

    lab utilitis exercise [sleep]

---
+++ user/sleep.c
@@ -0,0 +1,19 @@
+#include "kernel/types.h"
+#include "kernel/fcntl.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[]) {
+  int ticks;
+  if (argc <= 1) {
+    fprintf(2, "usgae: sleep <ticks>\n");
+    exit(1);
+  }
+  if (argc >= 3) {
+    fprintf(2, "too much args\n");
+    exit(1);
+  }
+  ticks = atoi(argv[1]);
+  sleep(ticks);
+  exit(0);
+}
